de222fdbc51aa4010175205f1077a78a581a9d1d
s2ahil <101473078+s2ahil@users.noreply.github.com>
2024-02-09 18:49:56 +0530

    Add files via upload

---
+++ app/page.js
@@ -0,0 +1,16 @@
+import Image from "next/image";
+import { Button } from "@/components/ui/button"
+
+
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+     
+   
+     <h1 className="">Ideas bro</h1>
+     <Button>Click me</Button>
+
+    </main>
+  );
+}
